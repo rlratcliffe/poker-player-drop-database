92b83826254b0aa603eac2a9a942128958d6a615
Rob Ratcliffe <rlr@tcliffe.dev>
2025-05-06 09:53:05 -0500

    reduce betting amount

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,14 +4,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class Player {
 
-    static final String VERSION = "1.3";
+    static final String VERSION = "1.4";
 
     public static int betRequest(JsonNode request) {
         System.out.println("Request output: " + request.toPrettyString());
-        return 1000;
+        return 10;
     }
 
     public static void showdown(JsonNode game) {
-        System.out.println("Showdown output: " + game.toPrettyString());
     }
 }
