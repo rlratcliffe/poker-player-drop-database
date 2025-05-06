947c0ebe6abbea131f7b62f0deaddfc16bdacca4
Rob Ratcliffe <rlr@tcliffe.dev>
2025-05-06 10:20:09 -0500

    check for pairs

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -13,9 +13,16 @@ public class Player {
     public static int betRequest(JsonNode request) {
         System.out.println("Request output: " + request.toPrettyString());
         System.out.println("Players: " + getPlayerByName(request.get("players")));
-        System.out.println("Players: " + getPlayerByName(request.get("players")).get("hole_cards"));
+        JsonNode holeCardsNode = getPlayerByName(request.get("players")).get("hole_cards");
+        System.out.println("Players: " + holeCardsNode);
+
+
+
+        if (isPair(holeCardsNode)) {
+            return 100;
+        }
         // 10 or higher
-        // pair
+
         return 10;
     }
 
@@ -38,5 +45,32 @@ public class Player {
                 .filter(player -> name.equals(player.get("name").asText()))
                 .findFirst()
                 .orElse(null);
+    }
+
+    public static boolean isPair(JsonNode cardsNode) {
+        String rank1 = "";
+        String rank2 = "";
+        int index = 0;
+        boolean isPair = false;
+        if (cardsNode.isArray()) {
+            for (JsonNode cardNode : cardsNode) {
+                String rank = cardNode.get("rank").asText();
+                System.out.println("Found card with rank: " + rank);
+
+                if (index == 0) {
+                    rank1 = rank;
+                } else {
+                    rank2 = rank;
+                }
+
+                index++;
+            }
+
+            if (rank1.equals(rank2)) {
+                System.out.println("Rank 1 == rank 2");
+                isPair = true;
+            }
+        }
+        return isPair;
     }
 }
