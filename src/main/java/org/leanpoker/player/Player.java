646f0c4b53c2d04e1c9448402fee9d40732fe6b8
Rob Ratcliffe <rlr@tcliffe.dev>
2025-05-06 09:44:22 -0500

    print out stuff

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class Player {
 
-    static final String VERSION = "1.2";
+    static final String VERSION = "1.3";
 
     public static int betRequest(JsonNode request) {
+        System.out.println("Request output" + request.toPrettyString());
         return 1000;
     }
 
     public static void showdown(JsonNode game) {
+        System.out.println("Showdown output: " + game.toPrettyString());
     }
 }
