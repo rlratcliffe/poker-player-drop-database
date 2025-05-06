fc8839034c5d4a8e3eea1ca0d29548f505292e9d
Rob Ratcliffe <rlr@tcliffe.dev>
2025-05-06 10:06:27 -0500

    try to print out data

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,15 +2,40 @@ package org.leanpoker.player;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.StreamSupport;
+
 public class Player {
 
     static final String VERSION = "1.4";
 
     public static int betRequest(JsonNode request) {
         System.out.println("Request output: " + request.toPrettyString());
+        System.out.println(getPlayerByName(request.get("players")));
+        // 10 or higher
+        // pair
         return 10;
     }
 
     public static void showdown(JsonNode game) {
+    }
+
+    public static JsonNode getPlayerByName(JsonNode playersNode) {
+        if (!playersNode.isArray()) {
+            System.out.println("Returned null from getPlayerByName");
+            return null;
+        }
+
+        String name = "Drop Database";
+
+        return StreamSupport.stream(
+                        Spliterators.spliteratorUnknownSize(
+                                playersNode.elements(),
+                                Spliterator.ORDERED),
+                        false)
+                .filter(player -> name.equals(player.get("name").asText()))
+                .findFirst()
+                .orElse(null);
     }
 }
