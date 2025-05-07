54f7393d3abbcadca805e8111f64eac38742c2f9
Rob Ratcliffe <rlr@tcliffe.dev>
2025-05-07 09:37:35 -0500

    Revert "Wrap logging with version and gameid"
    This reverts commit d2b74600777638ce98a61a24c0020cd0e6b997fd.

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -13,56 +13,48 @@ import java.util.stream.StreamSupport;
 public class Player {
 
     static final String VERSION = "1.20";
-    public static String gameId;
-
-
-    public static void printIt(String str) {
-        System.out.println("VERSION: " + VERSION +
-            " gameId: " + gameId + " content: " + str);
-    }
 
     public static int betRequest(JsonNode request) {
-        gameId = request.get("game_id");
-        printIt("Request output: " + request.toPrettyString());
-        printIt("Players: " + getPlayerByName(request.get("players")));
+        System.out.println("Request output: " + request.toPrettyString());
+        System.out.println("Players: " + getPlayerByName(request.get("players")));
         JsonNode holeCardsNode = getPlayerByName(request.get("players")).get("hole_cards");
 
         JsonNode communityCards = request.get("community_cards");
 
         JsonNode allCards = getAllCards(communityCards, holeCardsNode);
 
-        printIt("Players: " + holeCardsNode);
+        System.out.println("Players: " + holeCardsNode);
 
         int currentPlayer = request.get("in_action").asInt();
         int bet = request.get("players").get(currentPlayer).get("bet").asInt();
         int theCall = request.get("current_buy_in").asInt() - bet;
 
-        printIt("Current game_id" + request.get("game_id"));
+        System.out.println("Current game_id" + request.get("game_id"));
 
         int minimumRaise = 1;
         try {
             minimumRaise = request.get("minimum_raise").asInt();
         } catch (Exception e) {
-            printIt("An exception occurred accessing minimum raise" + e.getMessage());
+            System.out.println("An exception occurred accessing minimum raise" + e.getMessage());
         }
         int newRaise =  theCall + minimumRaise;
 
-        printIt("All cards " + allCards.toPrettyString());
+        System.out.println("All cards " + allCards.toPrettyString());
 
         // we are calling with higher cards and they are raising
         // look at logs of why w're calling with pairs instead of raising
 
         if (isPotentialFlush(allCards)) {
-            printIt("Is potential flush and raising " + newRaise);
+            System.out.println("Is potential flush and raising " + newRaise);
             return newRaise;
         }
         // straight
         // maybe care about only larger pairs
         if (is10OrHigher(allCards)) {
-            printIt("Is 10 or higher " + allCards.toPrettyString() + " " + theCall);
+            System.out.println("Is 10 or higher " + allCards.toPrettyString() + " " + theCall);
             return theCall;
         } else if (hasOneOrTwoPairs(allCards)) {
-            printIt("Has pairs, should be raising " + allCards.toPrettyString() + " " + newRaise);
+            System.out.println("Has pairs, should be raising " + allCards.toPrettyString() + " " + newRaise);
             return newRaise;
         }
         // fold, be more specific
@@ -75,7 +67,7 @@ public class Player {
 
     public static JsonNode getPlayerByName(JsonNode playersNode) {
         if (!playersNode.isArray()) {
-            printIt("Returned null from getPlayerByName");
+            System.out.println("Returned null from getPlayerByName");
             return null;
         }
 
@@ -113,7 +105,7 @@ public class Player {
         }
 
         boolean moreThanZeroPairs = pairCount > 0;
-        printIt("All cards " + allCards.toPrettyString() + " Has pairs: " + moreThanZeroPairs);
+        System.out.println("All cards " + allCards.toPrettyString() + " Has pairs: " + moreThanZeroPairs);
         return moreThanZeroPairs;
     }
 
@@ -124,16 +116,16 @@ public class Player {
             for (JsonNode cardNode : allCards) {
                 if (cardNode.has("rank") && isHighRank(cardNode)) {
                     String rank = cardNode.get("rank").asText();
-                    printIt("RAnk in check 10 or higher: " + rank);
+                    System.out.println("RAnk in check 10 or higher: " + rank);
                     hasHighRank.put(rank, hasHighRank.getOrDefault(rank, 0) + 1);
                 } else {
-                    printIt("Rank is not 10 or higher");
+                    System.out.println("Rank is not 10 or higher");
                 }
             }
         }
 
         boolean notEmpty = !hasHighRank.isEmpty();
-        printIt("All cards " + allCards.toPrettyString() + " Has high rank count: " + notEmpty);
+        System.out.println("All cards " + allCards.toPrettyString() + " Has high rank count: " + notEmpty);
         return notEmpty;
     }
 
@@ -142,12 +134,12 @@ public class Player {
 
         if (allCards.isArray() && !allCards.isEmpty()) {
             String suit = allCards.get(0).get("suit").asText();
-            printIt("Checking if all cards match suit: " + suit);
+            System.out.println("Checking if all cards match suit: " + suit);
 
             for (JsonNode cardNode : allCards) {
                 String currentSuit = cardNode.get("suit").asText();
                 if (!suit.equals(currentSuit)) {
-                    printIt("Found different suit: " + currentSuit);
+                    System.out.println("Found different suit: " + currentSuit);
                     hasAllSameSuit = false;
                     break; // Exit the loop as soon as we find a different suit
                 }
@@ -157,7 +149,7 @@ public class Player {
             hasAllSameSuit = false;
         }
 
-        printIt("All cards " + allCards.toPrettyString() + " Has all same suit: " + hasAllSameSuit);
+        System.out.println("All cards " + allCards.toPrettyString() + " Has all same suit: " + hasAllSameSuit);
         return hasAllSameSuit;
     }
 
